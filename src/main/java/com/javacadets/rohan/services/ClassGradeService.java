6f4274dc8a0a1515863595c6df6f2305e0edb214
amalonzo27 <amalonzo@yondu.com>
2022-12-16 10:22:39 +0800

    changes in grade sheet

--- src/main/java/com/javacadets/rohan/services/ClassGradeService.java
+++ src/main/java/com/javacadets/rohan/services/ClassGradeService.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,80 +24,66 @@ public class ClassGradeService {
     private StudentRepository studentRepository;
 
     public Map<String, Object> getClassGradeSheet(String code, int batch) throws ClassNotFoundException {
-//        double maxTotalQuizScore=0.0;
-//        double maxTotalExerciseScore=0.0;
-//        double TotalQuizScore=0.0;
-//        double TotalExerciseScore=0.0;
-//        double maxTotalProjectScore=0.0;
-//        double TotalProjectScore=0.0;
 
         Classs classs = this.classRepository.findActiveClass(code, batch).orElseThrow(() -> new ClassNotFoundException(code, batch));
 
         List<Student> students = classs.getStudents().stream().toList();
-        Map<String, Object> studentsRecord = new LinkedHashMap<>();
+        Map<String, Object> s = new LinkedHashMap<>();
         for(Student student: students) {
             Map<String, Object> record = new LinkedHashMap<>();
-            Map<String, Object> mQuizRecord = new LinkedHashMap<>();
+            List<Integer> mQuizRecord = new ArrayList<>();
             for (QuizRecord quizRecord: student.getQuizRecords()) {
-                mQuizRecord.put(quizRecord.getQuiz().getTitle(), quizRecord.getScore());
-//                maxTotalQuizScore = maxTotalQuizScore + quizRecord.getQuiz().getMaxScore();
-//                TotalQuizScore = TotalQuizScore + quizRecord.getScore();
+               mQuizRecord.add(quizRecord.getScore());
+
             }
-            Map<String, Object> mExerciseRecord = new LinkedHashMap<>();
+            List<Integer> mExerciseRecord = new ArrayList<>();
             for (ExerciseRecord exerciseRecord: student.getExerciseRecords()) {
-                mExerciseRecord.put(exerciseRecord.getExercise().getTitle(), exerciseRecord.getScore());
-//                maxTotalExerciseScore = maxTotalExerciseScore + exerciseRecord.getExercise().getMaxScore();
-//                TotalExerciseScore = TotalExerciseScore + exerciseRecord.getScore();
+                mExerciseRecord.add(exerciseRecord.getScore());
+
             }
-            Map<String, Object> mProjectRecord = new LinkedHashMap<>();
+            List<Integer> mProjectRecord = new ArrayList<>();
             for (ProjectRecord projectRecord: student.getProjectRecords()) {
-                mProjectRecord.put(projectRecord.getProject().getProjectId()+"", projectRecord.getScore());
-     //           maxTotalProjectScore = projectRecord.getProject().getMaxScore();
-     //           TotalProjectScore = projectRecord.getScore();
+
+                mProjectRecord.add(projectRecord.getScore());
             }
-
-//            double quizGrade =  ((TotalQuizScore/maxTotalQuizScore) * classs.getQuizPercentage()/100)*100;
-//            double ExerciseGrade = ((TotalExerciseScore/maxTotalExerciseScore) * classs.getExercisePercentage()/100)*100;
-//            double projectGrade = ((TotalProjectScore/maxTotalProjectScore) * classs.getProjectPercentage()/100)*100;
-
             record.put("quizzes", mQuizRecord);
             record.put("exercises", mExerciseRecord);
             record.put("project", mProjectRecord);
-          //  record.put("Tentative Grade",quizGrade+ExerciseGrade+projectGrade+classs.getAttendancePercentage());
-            studentsRecord.put(student.getEmail(), record);
+
+            s.put(student.getEmail(),record);
         }
-        return studentsRecord;
+        Map <String, Object> stud = new LinkedHashMap<>();
+        stud.put("students",s);
+        return stud;
     }
 
     public Map<String, Object> getStudentClassGradeSheet(String code, int batch) throws ClassNotFoundException {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         Classs classs = this.classRepository.findActiveClass(code, batch).orElseThrow(() -> new ClassNotFoundException(code, batch));
-        Student student = this.studentRepository.findByEmail(auth.getName()).orElseThrow(()-> new UserNotFoundException(auth.getName()));
+        Student student = this.studentRepository.findByEmail(auth.getName()).orElseThrow(() -> new UserNotFoundException(auth.getName()));
+
 
         Map<String, Object> record = new LinkedHashMap<>();
-        Map<String, Object> mQuizRecord = new LinkedHashMap<>();
-        for (QuizRecord quizRecord: student.getQuizRecords()) {
-            if (quizRecord.getQuiz().getClasss().equals(classs)) {
-                mQuizRecord.put(quizRecord.getQuiz().getTitle(), quizRecord.getScore());
-            }
+        List<Integer> mQuizRecord = new ArrayList<>();
+        for (QuizRecord quizRecord : student.getQuizRecords()) {
+            mQuizRecord.add(quizRecord.getScore());
+
         }
-        Map<String, Object> mExerciseRecord = new LinkedHashMap<>();
-        for (ExerciseRecord exerciseRecord: student.getExerciseRecords()) {
-            if (exerciseRecord.getExercise().getClasss().equals(classs)) {
-                mExerciseRecord.put(exerciseRecord.getExercise().getTitle(), exerciseRecord.getScore());
-            }
+        List<Integer> mExerciseRecord = new ArrayList<>();
+        for (ExerciseRecord exerciseRecord : student.getExerciseRecords()) {
+            mExerciseRecord.add(exerciseRecord.getScore());
+
         }
-        Map<String, Object> mProjectRecord = new LinkedHashMap<>();
-        for (ProjectRecord projectRecord: student.getProjectRecords()) {
-            if(projectRecord.getProject().getClasss().equals(classs)) {
-                mProjectRecord.put(projectRecord.getProject().getProjectId()+"", projectRecord.getScore());
-            }
+        List<Integer> mProjectRecord = new ArrayList<>();
+        for (ProjectRecord projectRecord : student.getProjectRecords()) {
+
+            mProjectRecord.add(projectRecord.getScore());
         }
         record.put("quizzes", mQuizRecord);
         record.put("exercises", mExerciseRecord);
         record.put("project", mProjectRecord);
 
-        return record;
 
+        return record;
     }
 }
